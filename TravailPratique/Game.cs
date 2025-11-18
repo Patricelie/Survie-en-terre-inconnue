87d7784aae0b7b995af4756a75e1a14adca2026c
AMINATADIAO <6289892@cstjean.qc.ca>
2025-11-18 10:54:44 -0500

    third

--- TravailPratique/Game.cs
+++ TravailPratique/Game.cs
@@ -8,5 +8,70 @@ namespace TravailPratique
 {
     internal class Game
     {
+        public static char[,] grid =
+        {
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+        };
+        public static int posX = 0;
+        public static int posY = 0;
+        public static Random rnd = new Random();
+
+        public static void DiscoverLand()
+        {
+
+            if (grid[posY, posX] == ' ')
+            {
+                char color = Convert.ToChar(rnd.Next(0, 7));
+                grid[posY, posX] = color;
+            }
+        }
+
+        public static void MoveUp()
+        {
+            if (posY > 0)
+            {
+                posY--;
+                DiscoverLand();
+            }
+        }
+        public static void MoveDown()
+        {
+           
+            if (posY < grid.GetLength(0) - 1)
+            {
+                posY++;
+                DiscoverLand();
+            }
+        }
+        public static void MoveLeft()
+        {
+            
+            if (posX > 0)
+            {
+                posX--;
+                DiscoverLand();
+            }
+        }
+        public static void MoveRight()
+        {
+            
+            if (posX < grid.GetLength(0) - 1)
+            {
+                posX++;
+                DiscoverLand();
+            }
+        }
+       
+            //alt + 219 █
+      
     }
 }
--- TravailPratique/View.cs
+++ TravailPratique/View.cs
@@ -8,5 +8,48 @@ namespace TravailPratique
 {
     internal class View
     {
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Menu Principal");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1. Jouer");
+            Console.WriteLine("2. Charger une partie");
+            Console.WriteLine("3. Sauvegarder une partie");
+            Console.WriteLine("4. Crédits");
+            Console.WriteLine("5. Quitter");
+            Console.WriteLine("------------------------------------");
+            Console.Write("Choisissez une option:");
+            string option = Console.ReadLine();
+        }
+        public static void DisplayGrid(char[,] grid, int posY, int posX)
+        {
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < grid.GetLength(1); x++)
+                {
+                    if (y == posY && x == posX)
+                    {
+                        Console.WriteLine('X');
+                    }
+                    else
+                    {
+                        Console.Write(grid[y, x]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void GenerateColor()
+        {
+    
+            if( color == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Green/*Foret*/;
+            }
+            else if( color == 1) {
+                Console.BackgroundColor = ConsoleColor.Blue/*Riviere*/;
+            }
     }
 }
